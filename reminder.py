e4ef48717ce5b6bb0900095686582dfd6da46027
Yoto Kim <yongtokim1215@gmail.com>
2023-07-27 23:28:11 -0700

    Added color

--- reminder.py
+++ reminder.py
@@ -4,11 +4,13 @@ import tkinter as tk
 
 def job():
     window = tk.Tk()
+    window.title("Eye Strain Reminder")
     window.withdraw()  
     popup = tk.Toplevel(window)
-    popup.geometry("200x50")  
-    tk.Label(popup, text="Look away from the screen!").pack()
-    window.after(2000, lambda: window.destroy())
+    popup.geometry("500x50")
+    popup.configure(bg = 'white')
+    tk.Label(popup, text="Look at something 20 feet away for 20 seconds!", fg = "red", bg="white").pack()
+    window.after(4000, lambda: window.destroy())
     window.mainloop()
 
 
